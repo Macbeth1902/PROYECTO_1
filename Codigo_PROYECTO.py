0300a75e2863fbd74287af17b38e0dcf6ed682f3
Macbeth1902 <macbeth.1902@gmail.com>
2025-03-31 00:52:17 -0600

    Add files via upload

--- Codigo_PROYECTO.py
+++ Codigo_PROYECTO.py
@@ -4,38 +4,39 @@
 import pandas as pd
 import numpy as np
 import plotly.express as px
+import matplotlib.pyplot as plt
 import yfinance as yf
 import datetime
 import streamlit as st
 from scipy.stats import kurtosis, skew
 
-## Definimos la configuración de la página
+## Configuración de la página
 st.set_page_config(page_title="📊 Análisis Financiero", layout="wide")
 
 st.title("📈 Análisis de datos de Google")
 st.write("🔍 En este proyecto, analizaremos el comportamiento de las acciones de Google desde el **01 de Enero de 2010** hasta la fecha actual.")
 
-## Definimos la función para obtener datos del precio de cierre
+## Función para obtener datos del precio de cierre
 @st.cache_data
 def obtener_datos(stocks):
     inicio = "2010-01-01"
     fin = datetime.datetime.today().strftime('%Y-%m-%d')
     df = yf.download(stocks, start=inicio, end=fin)[["Close"]]
-    df = df.reset_index()  ##Convertimos la fecha en columna (más tarde se requerirá para visualizar los datos)
+    df = df.reset_index()  ## Convertimos la fecha en columna
     return df
 
-## Definimos la función para calcular los rendimientos logarítmicos
+## Función para calcular los rendimientos diarios logarítmicos
 @st.cache_data
-def calcular_rendimientos(df):
+def rendimientos_logaritmicos(df):
     df_rendimientos = df.copy()
-    df_rendimientos["Returns"] = df_rendimientos["Close"].pct_change()
-    df_rendimientos = df_rendimientos.dropna().reset_index()  ##Reseteamos el índice 
+    df_rendimientos["Returns"] = np.log(df_rendimientos["Close"] / df_rendimientos["Close"].shift(1))
+    df_rendimientos = df_rendimientos.dropna().reset_index(drop=True)  # Eliminamos valores NaN
     return df_rendimientos
 
-## Definimos una carga de datos con spinner, para una mejor visualización
+## Carga de datos con spinner
 with st.spinner('⏳ Cargando datos...'):
     df = obtener_datos(['GOOGL'])
-    df_rendimientos = calcular_rendimientos(df)
+    df_rendimientos = rendimientos_logaritmicos(df)
 
 ## Calculamos las métricas estadísticas
 media = df_rendimientos["Returns"].mean()
@@ -53,3 +54,25 @@ with col2:
 
 with col3:
     st.metric(label="📉 Sesgo", value=f"{sesgo_valor:.2f}")
+
+## Elaboramos una gráfica con los rendimientos diarios:
+st.subheader("📈 Evolución de los rendimientos de Google")
+fg, ax = plt.subplots(figsize=(13, 5))
+ax.plot(df_rendimientos["Date"], df_rendimientos["Returns"], color="blue")
+ax.axhline(y=0, color='r', linestyle='--')
+ax.legend()
+ax.set_title("Evolución de los rendimientos de Google")
+ax.set_xlabel("Fecha")
+ax.set_ylabel("Rendimiento Diario")
+st.pyplot(fg)
+
+## Elaboramos un histograma de los rendimientos diarios del activo
+st.subheader("📊 Histograma de los rendimientos de Google")
+fig, ax = plt.subplots(figsize=(10, 5))
+ax.hist(df_rendimientos["Returns"], bins=50, color="blue", edgecolor="black")
+ax.axvline(x=media, color='r', linestyle='--', label=f"Media: {media:.5f}")
+ax.legend()
+ax.set_title("Histograma de los rendimientos de Google")
+ax.set_xlabel("Rendimiento Diario")
+ax.set_ylabel("Frecuencia")
+st.pyplot(fig)
