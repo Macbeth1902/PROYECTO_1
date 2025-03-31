44b6e4e21ed35e671850cd20b9d27797dc413a07
Macbeth1902 <macbeth.1902@gmail.com>
2025-03-31 02:46:02 -0600

    Add files via upload

---
+++ Homepage.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(page_title="📊 Proyecto I : Métricas de Riesgo", layout="wide")
+
+st.title("📌 Proyecto I de Métodos Cuantitativos en Finanzas")
+st.write("🔍 Bienvenido a nuestro proyecto de análisis financiero. En esta plataforma, exploraremos distintos aspectos de la acción de Google.")
+
+# Información del equipo
+st.header("👥 Integrantes del equipo")
+st.write("- Cerros García David")  
+st.write("- Flores Moreno Alan Alberto")  
+st.write("- Melgarejo Hernández Beatriz")
+st.write("- Tilihuit Ortigoza Rebeca Macbeth")  
+
+st.header("📖 Navegación")
+st.write("Utiliza la barra lateral para moverte entre las diferentes secciones de este análisis.")
